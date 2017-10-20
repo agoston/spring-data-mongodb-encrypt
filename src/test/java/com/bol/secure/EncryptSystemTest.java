934ea3375bd7ab455ee5eda4c65c93d7c8f4955a
Agoston Horvath <ahorvath@bol.com>
2017-10-20 10:46:52 +0200

    added system test for checking randomness of encryption

--- src/test/java/com/bol/secure/EncryptSystemTest.java
+++ src/test/java/com/bol/secure/EncryptSystemTest.java
@@ -2,7 +2,6 @@ package com.bol.secure;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import org.bson.types.Binary;
 import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,12 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
-/** needs mongodb running locally */
+/** needs mongodb running locally; fixme: use embedmongo */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {MongoDBConfiguration.class})
 public class EncryptSystemTest {
@@ -54,11 +56,33 @@ public class EncryptSystemTest {
 
     @Test
     public void consecutiveEncryptsDifferentResults() {
-        // fixme
+        MyBean bean = new MyBean();
+        bean.nonSensitiveData = "grass is green";
+        bean.secretData = "earth is flat";
+        mongoTemplate.save(bean);
+
+        DBObject fromMongo1 = mongoTemplate.getCollection(MyBean.MONGO_MYBEAN).find(new BasicDBObject("_id", new ObjectId(bean.id))).next();
+        byte[] cryptedSecret1 = (byte[]) fromMongo1.get(MyBean.MONGO_SECRETDATA);
+
+        mongoTemplate.save(bean);
+
+        DBObject fromMongo2 = mongoTemplate.getCollection(MyBean.MONGO_MYBEAN).find(new BasicDBObject("_id", new ObjectId(bean.id))).next();
+        byte[] cryptedSecret2 = (byte[]) fromMongo2.get(MyBean.MONGO_SECRETDATA);
+
+        assertThat(cryptedSecret1.length, is(cryptedSecret2.length));
+        // version
+        assertThat(cryptedSecret1[0], is(cryptedSecret2[0]));
+
+        // chances of having the same bytes in the same positions is negligible
+        int equals = 0;
+        for (int i = 1; i < cryptedSecret1.length; i++) {
+            if (cryptedSecret1[i] == cryptedSecret2[i]) equals++;
+        }
+
+        assertThat("crypted fields look too much alike", equals, is(not(greaterThan(cryptedSecret1.length / 10))));
     }
 
     // fixme: unit test for preparing the model via reflection
     // fixme: explode code into smaller parts/classes
-    // fixme:
 
 }
