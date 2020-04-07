799df0ed35dae1e9a5e89ddb668bc490a2202240
Agoston Horvath <ahorvath@bol.com>
2020-04-07 14:44:13 +0200

    apply JCE hack only on java8

--- src/main/java/com/bol/util/JCEPolicy.java
+++ src/main/java/com/bol/util/JCEPolicy.java
@@ -22,10 +22,13 @@ public final class JCEPolicy {
         // ...
     }
 
+    @Deprecated
     public static void allowUnlimitedStrength() {
         if (!hackApplied.compareAndSet(false, true)) return;
 
         try {
+            if (!needApply()) return;
+
             Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
             field.setAccessible(true);
 
@@ -34,8 +37,22 @@ public final class JCEPolicy {
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
             field.set(null, false);
-        } catch (IllegalAccessException | NoSuchFieldException | ClassNotFoundException e) {
+        } catch (Throwable e) {
             LOG.trace("Exception caught while trying to open JCE via reflection", e);
         }
+    }
+
+    public static boolean needApply() {
+        String discard, major, minor, update, build;
+
+        String[] javaVersionElements = System.getProperty("java.runtime.version").split("\\.|_|-b");
+
+        discard = javaVersionElements[0];
+        major = javaVersionElements[1];
+        minor = javaVersionElements[2];
+        update = javaVersionElements[3];
+        build = javaVersionElements[4];
+
+        return "1".equals(discard) && "8".equals(major);
     }
 }
